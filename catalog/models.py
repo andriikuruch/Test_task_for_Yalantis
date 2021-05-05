a823519ab897faf3db79861c46fddce1a5f5f9aa
andriikuruch <andriikuruch@gamil.com>
2021-05-05 13:21:53 +0300

    Add validation

--- catalog/models.py
+++ catalog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Course(models.Model):
     title = models.CharField(max_length=100, db_index=True)
     date_start = models.DateField()
-    date_finish = models.DateField()
+    date_end = models.DateField()
     number_of_lectures = models.PositiveIntegerField()
 
     def __str__(self):
--- catalog/serializers.py
+++ catalog/serializers.py
@@ -5,4 +5,16 @@ from .models import Course
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
-        fields = ('id', 'title', 'date_start', 'date_finish', 'number_of_lectures')
+        fields = ('id', 'title', 'date_start', 'date_end', 'number_of_lectures')
+
+    def validate(self, data):
+        errors = []
+
+        if data['date_start'] > data['date_end']:
+            errors.append({'date_end': 'Finish date must be occur after start.'})
+
+        if data['number_of_lectures'] < 0:
+            errors.append({'number_of_lectures': 'Number of lectures must be positive number.'})
+
+        if errors:
+            raise serializers.ValidationError(*errors)
